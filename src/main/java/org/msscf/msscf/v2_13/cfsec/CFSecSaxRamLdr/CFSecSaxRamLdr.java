78ba4402ec2b6c93e3bf42a6867c890671695122
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-15 13:30:53 -0600

    2026-03-16 13:30 CST Clean up licensing, integrate resource resolution via mcf.markhome.server for URLs, and generally clean up the 2.12 and 2.13 code bases

--- src/main/java/org/msscf/msscf/v2_13/cfsec/CFSecSaxRamLdr/CFSecSaxRamLdr.java
+++ src/main/java/org/msscf/msscf/v2_13/cfsec/CFSecSaxRamLdr/CFSecSaxRamLdr.java
@@ -5,26 +5,23 @@
  *
  *	Copyright (c) 2016-2026 Mark Stephen Sobkow
  *	
- *	MSS Code Factory CFSec 2.13 Security Essentials
+ *	Mark's Code Fractal CFSec 2.13 Security Services
  *	
- *	Copyright (C) 2016-2026 Mark Stephen Sobkow (mailto:mark.sobkow@gmail.com)
+ *	Copyright 2016-2026 Mark Stephen Sobkow
  *	
- *	This program is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ *	This file is part of Mark's Code Fractal CFSec.
  *	
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
  *	
- *	You should have received a copy of the GNU General Public License
- *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *	http://www.apache.org/licenses/LICENSE-2.0
  *	
- *	If you wish to modify and use this code without publishing your changes,
- *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
- *	for a commercial license at mark.sobkow@gmail.com
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
  *
  *	Manufactured by MSS Code Factory 2.12
  */
